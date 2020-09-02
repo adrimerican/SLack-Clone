f74dca35327141efdcf6c4d62952381cbe6b6855
Alex Arditti <alex.arditti@hotmail.com>
2020-09-02 15:00:46 +0100

    #1 Messaging
    Group messaging using class components stored as state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+//Previous group messages stored as state 
+  state = {
+    Messages: [
+      {message: "Hello how are you"},
+      {message: "I'm fine thank you"},
+      {message: "What are we doing today"},
+      ],
+      newMessage: '',
+      
+  }
+  //enters a new message from the input using a class component
+  enterMessage = (event) => {
+    this.setState ({
+      ...this.state,
+      newMessage: event.target.value,
+    })
+  }
+  //sends a message from the enter button using a class component
+  sendMessage = (event) => {
+    event.preventDefault()
+    this.setState ({
+      ...this.state,
+      Messages: [...this.state.Messages, {message: this.state.newMessage}]
+    })
+  }
+  //deletes messages using a button
+  deleteMessage = (event, index) => {
+    event.preventDefault()
+    this.setState({
+      ...this.state,
+      Messages: [...this.state.Messages].slice(index, -1)
+    })
+  }
+
+  render() {
+    //renders the hard coded mesages from the array and delete/save message buttons
+    const allMessages =  this.state.Messages.map( (sentMessage, index) => {
+      return <p key={index}>{sentMessage.message} 
+      <button onClick={this.deleteMessage}>Delete</button>
+      <button onClick={this.saveMessage}>Save Message</button></p>
+    })
+    return( 
+    // Div to display sent messages and new message input box/enter button
+      <>
+        <div className="messageDisplay">
+          {allMessages}
+        </div>
+        <div className="messageInput">
+          <input className="inputBox" type="text" name="newMessage" placeholder="Enter a message"
+          onChange={this.enterMessage}
+          />
+          <button onClick={this.sendMessage}>Enter</button>
+        </div>
+
+      </>
+    )
+  }
 }
 
 export default App;
