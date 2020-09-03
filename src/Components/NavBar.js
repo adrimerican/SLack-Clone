2c72e31d9198ef060f8136403519901a2c9851aa
Adri Roos <adri.roos>
2020-09-03 10:27:59 +0100

    #2 NavBar
    This is the first draft of the nav bar work.

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Toolbar.css'
+
+const toolbar = props => {
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">THE LOGO</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Users</a> </li>
+                    <li><a href="/">Messages</a> </li>
+                    <li><a href="/">Channels</a> </li>
+                    <li><a href="/"></a> </li>
+                    <li><a href="/"></a> </li>
+                    <li><a href="/"></a> </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
